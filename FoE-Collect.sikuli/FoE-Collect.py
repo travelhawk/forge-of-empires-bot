27005b96521743586e8342b740fdb01c25d1c2e8
Michael Falk <falk@humatects.de>
2019-12-18 17:37:17 +0100

    added closing of event news panels

--- FoE-Collect.sikuli/FoE-Collect.py
+++ FoE-Collect.sikuli/FoE-Collect.py
@@ -2,29 +2,29 @@ print "start"
 
 def startGame():
     App.open("chrome")
-    wait(1.25)
+    wait(1.5)
     paste("https://de.forgeofempires.com/")
     type(Key.ENTER)
-    wait(1)
-    
+    wait(1.25)
+
     #login
-    if exists("1527601417125.png"):
+    if exists("1576683227345.png"):
         click("1527601417125")
         wait(1)
         click("1527601615780.png")
-        wait(3)
+        wait(4.5)
 
 def collectResources(event):
     print "collect"
     for x in findAll(Pattern("1527582458451.png").similar(0.57)):
-        click(x.below(40))
+        click(x.below(50))
 	for y in findAll(Pattern("1527581487161.png").similar(0.57)):
-		click(y.below(40))
+		click(y.below(50))
 
 def collectMoney(event):
     print "money"
     for x in findAll(Pattern("1527582458451.png").similar(0.57)):
-        click(x.below(40))
+        click(x.below(50))
 
 def collectTools(event):
     print "tools"
@@ -34,12 +34,17 @@ def collectTools(event):
 def collectArmy(event):
     print "army"
     for x in findAll(Pattern("1527601979700.png").similar(0.57)):
-        click(x.below(40))
+        click(x.below(50))
 
 def collectGoods(event):
     print "goods"
     for x in findAll(Pattern("1527605472635.png").similar(0.57)):
-        click(x.below(40))
+        click(x.below(50))
+
+def closeEventNews(event):
+    print "close events"
+    for x in findAll("1576683891159.png"):
+        click(x)
 
 def showChangedInRegion(event):
     print "something changed in ", event.region
@@ -62,6 +67,7 @@ def isLoggedIn():
 if not isLoggedIn():
     startGame()
 
+onAppear(Pattern("1576683891159.png").similar(0.95), closeEventNews)
 onAppear(Pattern("1527582458451.png").similar(0.57), collectMoney)
 onAppear(Pattern("1527581487161.png").similar(0.57), collectTools)
 onAppear(Pattern("1527601979700.png").similar(0.57), collectArmy)
